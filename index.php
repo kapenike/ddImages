7263f81cfa0e3c14f74f99652297b638a2c4c8cf
Spencer <kapenike@gmail.com>
2025-05-04 19:14:53 -0600

    setup basic environment

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<html>
+<head>
+<!-- external scripts -->
+<script src="jsui.min.js" type="text/javascript"></script>
+
+<!-- application scripts -->
 <?php
-echo 'test';
+function getImportList($list, $dir) {
+	$files = scandir($dir);
+	forEach($files as $file) {
+		if ($file != '.' && $file != '..') {
+			if (is_dir($dir.'/'.$file)) {
+				$list = array_merge(getImportList($list, $dir.'/'.$file));
+			} else {
+				$list[] = $dir.'/'.$file;
+			}
+		}
+	}
+	return $list;
+}
+
+// import all scripts from "ui_scripts"
+forEach(getImportList([], './ui_scripts') as $file) {
+	echo '<script src="'.$file.'" type="text/javascript"></script>'."\r\n";
+}
 ?>
+
+<script>
+// init application (./ui_scripts/main.js)
+document.addEventListener("DOMContentLoaded", function() {
+	initStreamOverlay();
+});
+</script>
+
+<link rel="stylesheet" href="main.css">
+</head>
+<body id="body"></body>
+</html>
---
+++ ui_scripts/main.js
@@ -0,0 +1,3 @@
+function initStreamOverlay() {
+	console.log('hello world');
+}
