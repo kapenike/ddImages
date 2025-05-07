7daf9498b98f2f962d81a27b67f5be1d33bf125d
Spencer <kapenike@gmail.com>
2025-05-06 20:21:50 -0600

    add ajax and loader ... mostly recycled from a private project

---
+++ ui_scripts/ajax.js
@@ -0,0 +1,103 @@
+/*
+	method: 'POST' / 'GET' / etc?2025
+	url: 'url'
+	obj: object data to send, likely formToObj('form id')
+	callback: function to return data to
+	loader_id: overlay a loader on this id, removed on response
+	 ! - this should probably be removed after a settimeout to ensure logic completes first ... as well as allowing multiple loaders per page so actions can be done asyn ... as long as data changes to files are processed in a queue ... come back to this maybe
+*/
+function ajax(method, url, obj, callback, loader_id = null) {
+	
+	// form object to send
+	let form_data = new FormData();
+	
+	// loop form data and insert into form_data
+	let obj_keys = Object.keys(obj);
+	for (let i=0; i<obj_keys.length; i++) {
+		// append file
+		if (obj[obj_keys[i]].constructor === FileList) {
+			for (let i2=0; i2<obj[obj_keys[i]].length; i2++) {
+				form_data.append(obj_keys[i]+'_'+i2, obj[obj_keys[i]][i2]);
+			}
+		} else {
+			// if array, append all values separately to the same "name[]"
+			if (Array.isArray(obj[obj_keys[i]])) {
+				obj[obj_keys[i]].forEach(v => {
+					form_data.append(obj_keys[i], v);
+				});
+			} else {
+				// simple append
+				form_data.append(obj_keys[i], obj[obj_keys[i]]);
+			}
+		}
+	}
+	
+	// old skool ajax
+	let xhttp = new XMLHttpRequest();
+	xhttp.open(method, url);
+	
+	// used too often in my life not to include, separates ajax request from normal post
+	xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	xhttp.onload = function() {
+		
+		// if contained a loader, remove loading state
+		if (loader_id !== null) {
+			document.getElementById(loader_id).className = document.getElementById(loader_id).className.replace(' contains_loader', '');
+			document.getElementById('ajax_loader').remove();
+		}
+
+		// callback with: status(bool), response(object)
+		callback((this.readyState == 4 && this.status == 200), (this.responseText != '' ? JSON.parse(this.responseText) : null));
+	}
+	
+	// if loader id identified, add class and insert loader
+	if (loader_id !== null) {
+		
+		// ensure loader_id exists as an element, then append the required class name
+		let to_elem = Select('#'+loader_id);
+		if (to_elem) {
+			if (to_elem && to_elem.className.indexOf('contains_loader') === -1) {
+				to_elem.className += ' contains_loader';
+			}
+			
+			// insert loader
+			to_elem.appendChild(
+				Create('div', {
+					id: 'ajax_loader',
+					children: [
+						Create('img', {
+							src: '/loader.php'
+						})
+					]
+				})
+			);
+		}
+		
+	}
+	
+	// ship
+	xhttp.send(form_data);
+}
+
+// grab form input elements and convert their name->data pair to an object
+function formToObj(id) {
+	// prevent form submission through standard method
+	event.preventDefault();
+	
+	let form = Select('#'+id);
+	let form_obj = new FormData(form);
+	let data = Object.fromEntries(form_obj.entries());
+	
+	Object.keys(data).forEach(x => {
+		// edge cases:
+		if ('File' in window && data[x] instanceof File) {
+			// if file, it must be set directly from the element
+			data[x] = form.elements[x].files;
+		} else if (x.slice(-2) == '[]') {
+			// if array name field, must getAll
+			data[x] = form_obj.getAll(x);
+		}
+	});
+	
+	return data;
+}
--- ui_scripts/main.js
+++ ui_scripts/main.js
@@ -1,3 +1,31 @@
 function initStreamOverlay() {
-	console.log('hello world');
+	
+	// generate and insert testing area
+	Select('#body', {
+		innerHTML: '',
+		children: [
+			Create('form', {
+				id: 'test',
+				children: [
+					Create('div', {
+						className: 'block',
+						children: [
+							Create('input', {
+								name: 'test',
+								type: 'text',
+								value: 'test value'
+							}),
+							Create('button', {
+								innerHTML: 'Test',
+								onclick: function () {
+									ajax('POST', '/index.php', formToObj('test'), ()=>{}, 'test');
+								}
+							})
+						]
+					})
+				]
+			})
+		]
+	});
+	
 }
