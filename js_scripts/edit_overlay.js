3cc3a03d3a5f85f60e206ddefaec2135658ac31d
Spencer <kapenike@gmail.com>
2025-07-26 17:55:54 -0600

    fix bug for reference point on a non clip path group layer during grid movement

--- js_scripts/edit_overlay.js
+++ js_scripts/edit_overlay.js
@@ -1056,8 +1056,16 @@ function setupLayerInfo() {
 
 function moveGroupLayer(layer, obj) {
 	if (layer.type == 'clip_path') {
-		let x_diff = typeof obj.x !== 'undefined' ? obj.x - layer.clip_path.offset.x : 0;
-		let y_diff = typeof obj.y !== 'undefined' ? obj.y - layer.clip_path.offset.y : 0;
+		let x_diff = 0;
+		let y_diff = 0;
+		if (layer.clip_path.type != 'none') {
+			x_diff = typeof obj.x !== 'undefined' ? obj.x - layer.clip_path.offset.x : 0;
+			y_diff = typeof obj.y !== 'undefined' ? obj.y - layer.clip_path.offset.y : 0;
+		} else {
+			let layer_dim = getLayerOutputDimensions(layer);
+			x_diff = typeof obj.x !== 'undefined' ? obj.x - layer_dim.x : 0;
+			y_diff = typeof obj.y !== 'undefined' ? obj.y - layer_dim.y : 0;
+		}
 		layer.layers.forEach(sub_layer => {
 			sub_layer.offset.x += x_diff;
 			sub_layer.offset.y += y_diff;
